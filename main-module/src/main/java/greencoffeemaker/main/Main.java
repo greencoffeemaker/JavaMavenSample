b4bced178ad6ca0a3198aa9fdfbd0cd57faf6f9b
greencoffeemaker <greencoffeemaker@gmail.com>
2016-08-27 20:50:38 +0900

    Add Sub Class called by Main Class.

--- main-module/src/main/java/greencoffeemaker/main/Main.java
+++ main-module/src/main/java/greencoffeemaker/main/Main.java
@@ -1,9 +1,12 @@
 package greencoffeemaker.main;
 
+import greencoffeemaker.sub.SubClass;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Maven Building World !");
+		System.out.println(new SubClass().hello());
 	}
 
 }
---
+++ sub-module/src/main/java/greencoffeemaker/sub/SubClass.java
@@ -0,0 +1,13 @@
+package greencoffeemaker.sub;
+
+public class SubClass {
+
+	public SubClass() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public String hello() {
+		return "Hello Sub Class !";
+	}
+
+}
